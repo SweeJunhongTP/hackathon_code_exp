6e66006ac860f968e699a8a34c5eef66ec4eb740
SweeJunhongTP <2001043e@student.tp.edu.sg>
2022-06-13 22:36:23 +0800

    save work for CRU of apply leave

--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -2,88 +2,93 @@ import { StyleSheet, Text, View } from 'react-native'
 import React, { Component, useEffect, useState } from 'react'
 import { TouchableOpacity } from 'react-native-web'
 
-import {useNavigation} from '@react-navigation/core'
+import { useNavigation } from '@react-navigation/core'
 import { Calendar } from 'react-native-calendars';
 import { doc, getDoc } from 'firebase/firestore';
-import { auth ,db} from '../../firebase';
+import { auth, db } from '../../firebase';
 
 
 export default function HomeScreen() {
     const navigation = useNavigation()
     const [user, setUser] = useState(null)
-
-
-const getUser = async () => {
-  try {
     
-    const documentSnapshot = await getDoc(doc(db,'Users',auth.currentUser.uid))
-    const userData = documentSnapshot.data();
-   // console.log(userData)
-    setUser(userData);
+    const getUser = async () => {
+        try {
+            const documentSnapshot = await getDoc(doc(db, 'Users', auth.currentUser.uid))
+            const userData = documentSnapshot.data();
+            // console.log(userData)
+            setUser(userData);
+            const isAdmin = userData.role == 'Admin'
+            if(isAdmin){
+             setShowResults(true)
+            }        
+        } catch (error) {
+            //do whatever
+            alert(error.message)
+        }
+    };
 
-  } catch (error){
-    //do whatever
-    alert(error.message)
-  }
-};
-
-// Get user on mount
-useEffect(() => {
-  getUser();
-}, []);
-
-    const handleSignOut = () =>{
-        auth
-        .signOut()
-        .then(()=>{
-            navigation.replace("Login")
-        })
-        .catch(error => alert(error.message))
-    }
-
-  return (
-    <View style={styles.container}>
-        
-         <Text>{user && user?.fullName}</Text>
-    {/* <Text>Email: {auth.currentUser?.email}</Text>
-      <TouchableOpacity style={styles.button}
-      onPress={handleSignOut}>
-          <Text style ={styles.buttonText}>Sign out</Text>
-      </TouchableOpacity> */}
+    // Get user on mount
+    useEffect(() => {
+        getUser()
       
-      <View>
-        <Text style={styles.welcome}> Welcome to Smart RO!</Text>
-    </View>
+    }, []);
+    const [showResults, setShowResults] = React.useState(false)
+   
+    const handleSignOut = () => {
+        auth
+            .signOut()
+            .then(() => {
+                navigation.replace("Login")
+            })
+            .catch(error => alert(error.message))
+    }
+   
+    return (
+        <View style={styles.container}>
 
-    <TouchableOpacity style={styles.button}
-    onPress={() => navigation.navigate('ViewCalendar')}>
-        <Text style ={styles.buttonText}>View Calendar</Text>
-    </TouchableOpacity>
+            <Text>{user && user?.fullName}</Text>
+            {/* <Text>Email: {auth.currentUser?.email}</Text>
+     */}
 
-    <TouchableOpacity style={styles.button}
-    onPress={() => navigation.navigate('ApplyLeave')}>
-        <Text style ={styles.buttonText}>Apply for Leave</Text>
-    </TouchableOpacity>
+            <View>
+                <Text style={styles.welcome}> Welcome to Smart RO!</Text>
+            </View>
 
-    <TouchableOpacity style={styles.button}
-    onPress={() => navigation.navigate('ApproveLeave')}>
-        <Text style ={styles.buttonText}>Approve Leave</Text>
-    </TouchableOpacity>
+            <TouchableOpacity style={styles.button}
+                onPress={() => navigation.navigate('ViewCalendar')}>
+                <Text style={styles.buttonText}>View Calendar</Text>
+            </TouchableOpacity>
 
-    <TouchableOpacity style={styles.buttonsignout}
-      onPress={() => handleSignOut()}>
-          <Text style ={styles.signoutText}>Sign out</Text>
-      </TouchableOpacity>
-    </View>
-  )
+            <TouchableOpacity style={styles.button}
+                onPress={() => navigation.navigate('ApplyLeave')}>
+                <Text style={styles.buttonText}>Apply for Leave</Text>
+            </TouchableOpacity>
+
+           
+                    {showResults ?(
+                         <TouchableOpacity style={styles.button}
+                         onPress={() => navigation.navigate('ApproveLeave')}>
+                         <Text style={styles.buttonText}>Approve Leave</Text>
+                         </TouchableOpacity>
+                    ):null}
+           
+
+            <TouchableOpacity style={styles.buttonsignout}
+                onPress={() => handleSignOut()}>
+                <Text style={styles.signoutText}>Sign out</Text>
+            </TouchableOpacity>
+            
+        </View>
+    )
 }
 
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        justifyContent:'center',
-        alignItems:'center',
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
         backgroundColor: '#F4FBFF'
     },
     welcome: {
@@ -91,28 +96,28 @@ const styles = StyleSheet.create({
         fontWeight: '600',
         color: '#8D8D8D'
     },
-    button:{
-        backgroundColor:'#75C6EE',
-        width:'60%',
+    button: {
+        backgroundColor: '#75C6EE',
+        width: '60%',
         padding: 30,
         borderRadius: 10,
         alignItems: 'center',
         marginTop: 40,
     },
-    buttonsignout:{
-        backgroundColor:'#75C6EE',
-        width:'30%',
-        padding:25,
+    buttonsignout: {
+        backgroundColor: '#75C6EE',
+        width: '30%',
+        padding: 25,
         borderRadius: 10,
         alignItems: 'center',
         marginTop: 40,
     },
-    buttonText:{
+    buttonText: {
         color: 'white',
         fontWeight: '700',
         fontSize: 28
     },
-    signoutText:{
+    signoutText: {
         color: 'white',
         fontWeight: '700',
         fontSize: 24
